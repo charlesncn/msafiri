bbcc6c5d66aa2c49ee86587f3907fe65cfe2b1c8
charlesncn <charowcjn@gmail.com>
2023-07-07 09:46:10 +0300

    Fix: Loading app context, Feature: Integration test save product

--- src/test/java/com/msafiri/product/ProductApplicationTests.java
+++ src/test/java/com/msafiri/product/ProductApplicationTests.java
@@ -1,13 +1,71 @@
 package com.msafiri.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msafiri.product.dto.request.ProductRequest;
+import com.msafiri.product.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest()
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.3");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @DynamicPropertySource
+    static void setProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldSaveProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        String request = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isCreated());
+
+
+        Assertions.assertTrue(productRepository.findAll().size() > 0);
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("Test Product")
+                .description("Test Product Description")
+                .price(100.00)
+                .category("Test Category")
+                .image("Test Image")
+                .quantity(10)
+                .build();
+    }
 
 }
