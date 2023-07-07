7ce1e5a07f5c2106b1f13adf2e61dc0af3defb41
charlesncn <charowcjn@gmail.com>
2023-07-07 11:48:00 +0300

    Feature Test Delete endpoint

--- src/test/java/com/msafiri/product/ProductApplicationTests.java
+++ src/test/java/com/msafiri/product/ProductApplicationTests.java
@@ -2,8 +2,11 @@ package com.msafiri.product;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.msafiri.product.dto.request.ProductRequest;
+import com.msafiri.product.model.Product;
 import com.msafiri.product.repository.ProductRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -18,6 +21,9 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest()
@@ -28,6 +34,7 @@ class ProductApplicationTests {
 
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.3");
+//
 
     @Autowired
     private MockMvc mockMvc;
@@ -41,6 +48,9 @@ class ProductApplicationTests {
     @DynamicPropertySource
     static void setProps(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("spring.data.mongodb.socketKeepAlive", () -> true);
+
+
     }
 
 
@@ -75,8 +85,31 @@ class ProductApplicationTests {
                 .build();
     }
 
-    @AfterAll
-    static void tearDown() {
-        mongoDBContainer.close();
+    @Test
+    void shouldDeleteProduct() {
+        ProductRequest productRequest = getProductRequest();
+        productRepository.save(Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(BigDecimal.valueOf(productRequest.getPrice()))
+                .category(productRequest.getCategory())
+                .image(productRequest.getImage())
+                .quantity(String.valueOf(productRequest.getQuantity()))
+                .build());
+
+        Optional<Product> productOptional = productRepository.findAll().stream().findFirst();
+        productOptional.ifPresent(product -> {
+            try {
+                System.out.printf("Deleting product with id %s%n", product.getProductId());
+                mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/product")
+                                .param("id", product.getProductId())
+                                .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
     }
+
 }
