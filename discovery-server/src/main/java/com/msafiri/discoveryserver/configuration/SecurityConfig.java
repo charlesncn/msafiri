358476e535b9ce9171e383718f1824c6c4c0f8aa
charlesncn <charowcjn@gmail.com>
2023-07-20 11:53:23 +0300

    Fix: Remove Discovery service auth

--- discovery-server/src/main/java/com/msafiri/discoveryserver/configuration/SecurityConfig.java
+++ discovery-server/src/main/java/com/msafiri/discoveryserver/configuration/SecurityConfig.java
@@ -5,41 +5,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
-    @Value("${eureka.client.username}")
-    private String username;
-    @Value("${eureka.client.password}")
-    private String password;
-
-
-@Bean
-public InMemoryUserDetailsManager userDetailsService() {
-    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    UserDetails user = User.withUserDetails(User.builder()
-            .passwordEncoder(encoder::encode)
-            .username(username)
-            .password(password)
-            .roles("USER")
-            .build()).build();
-    return new InMemoryUserDetailsManager(user);
-}
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((auth) -> auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().ignoringRequestMatchers("/eureka/**");
         return http.build();
     }
 }
