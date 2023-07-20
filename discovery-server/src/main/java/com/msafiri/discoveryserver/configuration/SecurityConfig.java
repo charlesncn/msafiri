3d49895e9842a3e3f1bc4ffd41b57cac1cfbfb86
charlesncn <charowcjn@gmail.com>
2023-07-20 10:37:38 +0300

    Testing endpoint auth

--- discovery-server/src/main/java/com/msafiri/discoveryserver/configuration/SecurityConfig.java
+++ discovery-server/src/main/java/com/msafiri/discoveryserver/configuration/SecurityConfig.java
@@ -1,24 +1,45 @@
 package com.msafiri.discoveryserver.configuration;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+    @Value("${eureka.client.username}")
+    private String username;
+    @Value("${eureka.client.password}")
+    private String password;
 
+
+@Bean
+public InMemoryUserDetailsManager userDetailsService() {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    UserDetails user = User.withUserDetails(User.builder()
+            .passwordEncoder(encoder::encode)
+            .username(username)
+            .password(password)
+            .roles("USER")
+            .build()).build();
+    return new InMemoryUserDetailsManager(user);
+}
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withUserDetails(User.builder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build()).build();
-        return new InMemoryUserDetailsManager(user);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((auth) -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
